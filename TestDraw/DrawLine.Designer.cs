958e5c1e0e83cbb65ba4d1356f0fae0ad48a4b54
code test <AyahAdel25@gmail.com>
2020-04-22 20:12:02 +0200

    initial commit

---
+++ TestDraw/DrawLine.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace TestDraw
+{
+	partial class DrawLine
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnDrawLine = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnDrawLine
+			// 
+			this.btnDrawLine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+			this.btnDrawLine.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnDrawLine.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.btnDrawLine.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+			this.btnDrawLine.Location = new System.Drawing.Point(12, 315);
+			this.btnDrawLine.Name = "btnDrawLine";
+			this.btnDrawLine.Size = new System.Drawing.Size(151, 85);
+			this.btnDrawLine.TabIndex = 0;
+			this.btnDrawLine.Text = "Draw Line";
+			this.btnDrawLine.UseVisualStyleBackColor = false;
+			this.btnDrawLine.Click += new System.EventHandler(this.btnDrawLine_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(814, 422);
+			this.Controls.Add(this.btnDrawLine);
+			this.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "Form1";
+			this.Text = "Draw";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnDrawLine;
+	}
+}
+
---
+++ TestDraw/DrawLine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestDraw
+{
+	public partial class DrawLine : Form
+	{
+		public DrawLine()
+		{
+			InitializeComponent();
+		}
+
+		private void btnDrawLine_Click(object sender, EventArgs e)
+		{
+			Point p1 = new Point(50, 60);
+			Point p2 = new Point(300, 60);
+			this.CreateGraphics().DrawLine(Pens.Red, p1, p2);
+		}
+	}
+}
